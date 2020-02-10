d392a062ec3c834db780c4b0cefc2da1b26657e3
Fredrik Öhrström <fredrik@ohrstrom.io>
2020-02-10 22:00:22 +0100

    add startswith ramme check

--- src/ramme/commandParser.ts
+++ src/ramme/commandParser.ts
@@ -1,6 +1,7 @@
 export const commandParser = (message: string) => {
   let command
   try {
+    if (!message.startsWith('/ramme')) return undefined
     command = message.split(' ')[1]
   } catch (error) {}
   return command
