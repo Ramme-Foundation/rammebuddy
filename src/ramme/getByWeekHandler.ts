7f70b12f81f0c6d5b7bf26b789c36467ae48e899
Fredrik Öhrström <fredrik@ohrstrom.io>
2020-02-12 22:27:45 +0100

    weekHandler now reduces the entire week and formats it

--- src/ramme/getByWeekHandler.ts
+++ src/ramme/getByWeekHandler.ts
@@ -1,7 +1,57 @@
-import { Repository } from '../core'
+import { Repository, Event } from '../core'
 import { Ramme } from '.'
 import { getCurrentWeekNumber } from '../utils'
 import { Request, Response } from 'express'
+import { reduceRamme } from './reduceRamme'
+
+type ReducedWeek = { [key: string]: string[] }
+
+export const getByWeekHandler = async (
+  req: Request,
+  res: Response,
+  repository: Repository<Ramme>,
+) => {
+  const week = getWeekInMessage(req.body.text) || getCurrentWeekNumber()
+  const events = await repository.getByWeek(week)
+
+  const formattedWeek = formatWeek(reduceWeek(events))
+
+  res.send(formattedWeek)
+}
+
+const formatWeek = (week: ReducedWeek) => {
+  const warriors = Object.keys(week).map(committer => {
+    const activities = week[committer]
+    return `${committer} (${activities.length}) : [${activities}]`
+  })
+  return warriors.join('\n')
+}
+
+const reduceWeek = (events: Event<Ramme>[]) => {
+  const ids = events.map((e: Event<Ramme>) => e.id)
+  const unique = [...new Set(ids)]
+
+  const rammesBy: ReducedWeek = {}
+
+  unique.forEach(id => {
+    const ramme = events.filter((e: Event<Ramme>) => e.id === id)
+
+    const reducedRamme = reduceRamme(ramme)
+
+    if (reducedRamme?.activity) {
+      const committer = ramme[0].committer
+      const activity = reducedRamme.activity
+
+      if (Object.prototype.hasOwnProperty.call(rammesBy, committer)) {
+        rammesBy[committer].push(activity)
+      } else {
+        rammesBy[committer] = [activity]
+      }
+    }
+  })
+
+  return rammesBy
+}
 
 const getWeekInMessage = (text: string | undefined) => {
   if (!text) return undefined
@@ -16,15 +66,4 @@ const getWeekInMessage = (text: string | undefined) => {
   } catch (error) {
     return undefined
   }
-}
-
-export const getByWeekHandler = async (
-  req: Request,
-  res: Response,
-  repository: Repository<Ramme>,
-) => {
-  const week = getWeekInMessage(req.body.text) || getCurrentWeekNumber()
-  const eventRows = await repository.getByWeek(week)
-
-  res.send(eventRows)
 }
