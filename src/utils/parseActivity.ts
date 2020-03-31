07e44b3a1dea7f44110c1e0fb725b80f9b215a8d
Ante Wall <antewall@gmail.com>
2020-03-31 08:23:43 +0200

    Fix add message parsing

--- src/utils/parseActivity.ts
+++ src/utils/parseActivity.ts
@@ -9,6 +9,12 @@ export const parseActivity = (message: string): Activity => {
   const WEEK_REGEX = /(?:vecka|week)?\s*(\d+)/g
   const match = WEEK_REGEX.exec(message)
   let week = getCurrentWeekNumber()
+  if (match === null) {
+    return {
+      week,
+      message,
+    }
+  }
   if (match?.length === 2) {
     week = Number(match![1].trim())
   }
